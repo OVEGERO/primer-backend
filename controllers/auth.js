7bd1be6c04db8f3d0c92f1ef19ecf9b11e8107ce
OVEGERO <rat4355@gmail.com>
2023-02-27 22:28:26 -0500

    Fin seccion 11 - version 4.0.0

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 import { response } from "express"; 
 import Usuario from '../models/user.js';
 import bcrypt from 'bcryptjs';
+import { googleVerify } from '../helpers/google-verify.js';
 import { generarJWT } from "../helpers/generarJWT.js";
 
 export const login = async(req, res=response) => {
@@ -53,6 +54,61 @@ export const login = async(req, res=response) => {
   res.json({
     msg: 'Login ok'
   });
+
+};
+
+export const googleSignIn = async(req, res=response) => {
+
+  const { id_token } = req.body;
+
+  try {
+    
+    const { nombre, img, correo } = await googleVerify( id_token );
+
+    let usuario = await Usuario.findOne({ correo });
+
+    if ( !usuario ) {
+
+      const data = {
+        nombre,
+        correo,
+        password: '123456',
+        img,
+        google: true, 
+        estado: true,
+        rol: 'USER_ROLE'
+      };
+
+      usuario = new Usuario( data );
+
+      await usuario.save();
+
+    }
+
+    if ( !usuario.estado ) {
+      return res.status(401).json({
+        msg: 'Hable con el administrador, usuario bloqueado'
+      });
+    }
+
+    const token = await generarJWT( usuario.id );
+
+    return res.json({
+      usuario,
+      token
+    });
+    
+
+  } catch (error) {
+
+    console.log(error);
+    
+    return res.status(400).json({
+      ok: false,
+      msg: 'Token de Google no es valido'
+    });
+
+  }
 
 };
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { body, param } from 'express-validator';
-import { login } from '../controllers/auth.js';
+import { googleSignIn, login } from '../controllers/auth.js';
 import { validarCampos } from '../middlewares/validar-campos.js';
 
 export const routerAuth = Router();
@@ -11,3 +11,7 @@ routerAuth.post('/login', [
     validarCampos
 ], login);
 
+routerAuth.post('/google', [
+    body('id_token', 'El id_token es necesario').not().isEmpty(),
+    validarCampos
+], googleSignIn);
